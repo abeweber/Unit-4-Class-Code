f37282df7a3929dfa447d1e85d0639239dbeb52b
smithbwlhs <smithb@wlwv.k12.or.us>
2024-11-04 00:19:53 -0800

    updated

---
+++ wrap_up_day.py
@@ -0,0 +1,26 @@
+"""
+Name: Mr.Smith
+Date: 11/4/2024
+Description: Nested Loop Practice Problems
+"""
+
+# Problem 1
+# Make a 10x10 multiplication table
+#   1 2 3 4 ...
+# 1 1 2 3 4 ...
+# 2 2 4 6 8 ...
+# . . . . . ...
+# . . . . . ...
+
+# Problem 2
+# Ask a user for 2 numbers and make a multiplication table of that size
+
+# Problem 3
+# modify the code to print the first 100 digits of pi
+with open("million_digits_pi.txt","r") as f:
+    count = 0
+    digits = ["0123456789"]
+    print(f.read(6))
+
+
+
