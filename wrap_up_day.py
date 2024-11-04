9e2ef4053356a09e957dc2dd385b353346dda137
smithbwlhs <smithb@wlwv.k12.or.us>
2024-11-04 09:40:58 -0800

    updated

--- wrap_up_day.py
+++ wrap_up_day.py
@@ -6,11 +6,35 @@ Description: Nested Loop Practice Problems
 
 # Problem 1
 # Make a 10x10 multiplication table
+# 1 2 3 4  5  6  7  8  9 10
+# 2 4 6 8 10 12 14 16 18 20
+# 3 6 9 ...........
+# etc etc
+for row in range(1,11):
+    for column in range(1,11):
+        print(f"{str(row*column):<4}",end = "") # this makes each product take up 3 characters
+
+    print()
+#    OR
+
 #   1 2 3 4 ...
 # 1 1 2 3 4 ...
 # 2 2 4 6 8 ...
 # . . . . . ...
 # . . . . . ...
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Problem 2
 # Ask a user for 2 numbers and make a multiplication table of that size
